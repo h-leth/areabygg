f4c1c461ff84bb9191e1ee6e7c88bbf7763dead7
Henning Leth <leth.henning@gmail.com>
2023-04-06 23:10:34 +0200

    add .htaccess

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = config => {
   config.addPassthroughCopy('./src/images/')
   config.addPassthroughCopy('./src/css/')
+  config.addPassthroughCopy('./src/.htaccess')
   return {
     markdownTemplateEngine: 'njk',
     dataTemplareEngine: 'njk',
